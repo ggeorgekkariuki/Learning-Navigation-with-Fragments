f46bf545909b2a1b1de711259cea63361efb6d0c
ggeorgekkariuki <gconsummer.gk@gmail.com>
2020-05-30 21:53:57 +0300

    Adding button to the Fragment and passing an intent to a new activity

--- app/src/main/java/com/example/learningnavigationwithfragments/ui/accounts/AccountsFragment.java
+++ app/src/main/java/com/example/learningnavigationwithfragments/ui/accounts/AccountsFragment.java
@@ -2,6 +2,7 @@ package com.example.learningnavigationwithfragments.ui.accounts;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,7 +12,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.learningnavigationwithfragments.Accounts;
 import com.example.learningnavigationwithfragments.R;
 
 public class AccountsFragment extends Fragment {
@@ -26,7 +29,18 @@ public class AccountsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_accounts, container, false);
+        View view = inflater.inflate(R.layout.fragment_accounts, container, false);
+//        Find the button within the layout fragment_accounts
+        Button button = view.findViewById(R.id.button_next);
+//        Set an click listener
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Pass an intent to a new Activity
+                startActivity(new Intent(getContext(), Accounts.class));
+            }
+        });
+        return view;
     }
 
     @Override
